582d85038ae581096c5404bd3deb08e417d5b7d3
Edgar Azevedo <edgarvazevedo@gmail.com>
2023-05-21 19:02:27 +0100

    novas configurações

--- config/db.config.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-// Conexão com o banco de dados MongoDB
-mongoose.connect("mongodb://localhost/todo_app", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
